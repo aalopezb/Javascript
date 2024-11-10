ca040efcd1ef704fdef49c51c36f48708fc5f207
aalopezb <a.alexander.l.b@gmail.com>
2024-11-10 13:17:19 -0500

    feat: :sparkles: Changes for heroku

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js!');
+  res.send('Hello from Heroku!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
